8777a9f8e468c532abb6beb6a047ace46823f9f4
xusl98 <jesussainz98@gmail.com>
2020-07-01 20:30:15 +0200

    cambios y comienzo servicios

--- js/common.js
+++ js/common.js
@@ -4,12 +4,48 @@ $(document).ready(function(){
     $('#hamb').click(function(){
         $('#menu').css('display', 'block');
         $('#menu').addClass('menuAppear');
+        setTimeout(function(){ 
+            $('#serviciosItem').css('display', 'block');
+            $('#serviciosItem').animate({ 'marginTop': '0' }, 1500);
+            // $('#serviciosItem').addClass('servicioItemAppear');
+         }, 1500);
+        setTimeout(function(){ 
+            $('#portfolioItem').css('display', 'block');
+            $('#portfolioItem').animate({ 'marginTop': '0' }, 1500);
+            // $('#serviciosItem').addClass('servicioItemAppear');
+         }, 1700);
+        setTimeout(function(){ 
+            $('#nosotrosItem').css('display', 'block');
+            $('#nosotrosItem').animate({ 'marginTop': '0' }, 1500);
+            // $('#serviciosItem').addClass('servicioItemAppear');
+         }, 1900);
+        setTimeout(function(){ 
+            $('#contactoItem').css('display', 'block');
+            $('#contactoItem').animate({ 'marginTop': '0' }, 1500);
+            // $('#serviciosItem').addClass('servicioItemAppear');
+         }, 2100);
+         
     });
     
     //close menu
     $('#exitMenu').click(function(){
         $('#menu').css('display', 'none');
+        $( ".menuText" ).stop();
+        $('#serviciosItem').css('margin-top', '10%');
+        $('#portfolioItem').css('margin-top', '15%');
+        $('#contactoItem').css('margin-top', '20%');
+        $('#nosotrosItem').css('margin-top', '25%');
         $('#menu').removeClass('menuAppear');
+        $('.menuText').css('display', 'none');
+    });
+    
+
+    $('.menuText').mouseover(function () {
+        $(this).find('path').attr('fill', 'white');
+    });
+
+    $('.menuText').mouseout(function () {
+        $(this).find('path').attr('fill', 'none');
     });
 
 });
