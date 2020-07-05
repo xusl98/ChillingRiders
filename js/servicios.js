15dfd0cb983ede2948f54c30daea1b8e1667f05f
xusl98 <jesussainz98@gmail.com>
2020-07-05 18:04:28 +0200

    terminado +-

--- js/servicios.js
+++ js/servicios.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+
+
+    var myObject = $("#dGraficoInv");
+    var myposition = myObject.offset();
+    
+    $("#dGrafico").css({  'left' : myposition.left , 'width' : $("#dGraficoInv").css('width') });
+    $("#dGrafico").offset({left:myposition.left})
+    
+    var myObject = $("#socialMediaInv");
+    var myposition = myObject.offset();
+    
+    $("#socialMedia").css({  'left' : myposition.left , 'width' : $("#socialMediaInv").css('width') });
+    $("#socialMedia").offset({left:myposition.left})
+    
+    var myObject = $("#dWebInv");
+    var myposition = myObject.offset();
+    
+    $("#dWeb").css({  'left' : myposition.left , 'width' : $("#dWebInv").css('width') });
+    $("#dWeb").offset({left:myposition.left})
+    
+    var myObject = $("#asesoriaInv");
+    var myposition = myObject.offset();
+    
+    $("#asesoria").css({  'left' : myposition.left , 'width' : $("#asesoriaInv").css('width') });
+    $("#asesoria").offset({left:myposition.left})
+    
+
+
+
+
     $('.titleUp').animate({ 'marginTop': '0%' }, 600);
     $('.titleDown').animate({ 'marginTop': '0%' }, 600, function (){
         $('.titleUpInv').css('visibility', 'visible');
